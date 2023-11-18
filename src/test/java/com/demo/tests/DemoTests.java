59c103485bd7dd4c12cd0b0e2378206e4e4b4b40
zk <zuhalkaled@gmail.com>
2023-11-17 20:09:18 -0500

    updated

--- src/test/java/com/demo/tests/DemoTests.java
+++ src/test/java/com/demo/tests/DemoTests.java
@@ -22,41 +22,38 @@ public class DemoTests extends TestBase {
     public void test3() {
         System.out.println("Test -3");
     }
-//
-////dependsOn
-//    //skip a test
-////    @Test (enabled = false) //ignores this test
-////    public void testSkipped(){
-////        System.out.println("This test skipped");
-////    }
-//
-//    //Parameterization
-//    @Parameters({"browser"})
-//    @Test
-//    public void parameterTest(String browser){
-//        System.out.println(browser);
-//    }
-//
-//    //Data provider
-//
-//    @Test (dataProvider = "data-provider")
-//    public void dpTest(String val){
-//        System.out.println(val);}
-//
-//    @DataProvider(name = "data-provider")
-//    public Object[][] dataProvider(){
-//    return new Object[][] {{"hello"}, {"there"},{"Danil"}};}
-//
-//
-//    //Exception handling
-//    @Test (expectedExceptions = {ArithmeticException.class, TimeoutException.class})
-//    public void testException(){
-//        System.out.println("Test - Exception");
-//        System.out.println(2/0);
-//    }
-//
-////    junit: @DisplayName("Login_Test"), testng doesn't allow a custom method name
-//
-//
-//}
+
+    //skip a test
+    @Test (enabled = false) //ignores this test
+    public void testSkipped(){
+        System.out.println("This test skipped");
+    }
+
+    //Parameterization
+    @Parameters({"browser"})
+    @Test
+    public void parameterTest(String browser){
+        System.out.println(browser);
+    }
+
+    //Data provider
+
+    @Test (dataProvider = "data-provider")
+    public void dpTest(String val){
+        System.out.println(val);}
+
+    @DataProvider(name = "data-provider")
+    public Object[][] dataProvider(){
+    return new Object[][] {{"hello"}, {"there"},{"this is a demo project"}};}
+
+
+    //Exception handling
+    @Test (expectedExceptions = {ArithmeticException.class})
+    public void testException(){
+        System.out.println("Test - Exception");
+        System.out.println(2/0);
+    }
+
+//    junit: @DisplayName("Login_Test"), testng doesn't allow a custom method name
+
 }
